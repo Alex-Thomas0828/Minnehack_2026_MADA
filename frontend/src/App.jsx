e5971215efbaca187b627a368fc110b967bff711
“Mudit <jha00051@umn.edu>
2026-02-15 11:04:35 -0600

    ui polish on map

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import { supabase } from "./lib/supabase";
 import MendMap from "./components/Map";
 import AuthPage from "./pages/AuthPage";
 import MendDetails from "./components/MendDetails";
+import { Plus } from 'lucide-react';
 
 function App() {
   const [pins, setPins] = useState([]);
@@ -95,20 +96,21 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        
+
         {/* Auth Page */}
         <Route path="/auth" element={<AuthPage />} />
 
         {/* Protected Map Page */}
         <Route
           path="/"
-          element={
+          element={   
             user ? (
               <div className="flex flex-col h-screen w-screen overflow-hidden">
                 <header className="bg-mend-dark p-4 flex justify-between items-center shadow-md z-[1000]">
                   <h1 className="text-mend-white text-xl font-bold">The MSP Mend</h1>
-                  <button className="bg-mend-cyan text-mend-dark px-4 py-2 rounded-full font-bold text-sm">
-                    + Drop a Pin
+                  <button className="bg-mend-white text-mend-dark px-4 py-2 rounded-full font-bold text-sm border-2 border-transparent hover:bg-mend-cyan transition hover:border-2  hover:text-mend-white cursor-pointer shadow-sm flex gap-2 text-center items-center">
+                    <Plus />
+                    DROP A PIN
                   </button>
                 </header>
 
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -5,61 +5,61 @@ import { Info } from 'lucide-react';
 import { getCurrentUser, getUserProfile } from '../lib/auth';
 
 export default function Sidebar() {
-    const [activePanel, setActivePanel] = useState(null);
-    const [user, setUser] = useState(null);
+  const [activePanel, setActivePanel] = useState(null);
+  const [user, setUser] = useState(null);
 
-    useEffect(() => {
-      const fetchUser = async () => {
-        const authUser = await getCurrentUser();
-        if (!authUser) return;
-    
-        const profile = await getUserProfile(authUser.id);
-        setUser(profile);
-      };
-      fetchUser();
-    }, []);
+  useEffect(() => {
+    const fetchUser = async () => {
+      const authUser = await getCurrentUser();
+      if (!authUser) return;
 
-    function togglePanel(panel) {
-      setActivePanel(prev => (prev === panel ? null : panel));
-    }
-  
-    return (
-      <div className="flex h-screen relative">
-  
-        {/* SIDEBAR */}
-        <div className="w-20 bg-[var(--color-mend-dark)] text-white flex flex-col items-center py-6">
-          <div className="flex flex-col space-y-6 w-full items-center">
-  
-            <button
-              className="flex items-center justify-center w-12 h-12 rounded-lg hover:bg-gray-800"
-              onClick={() => togglePanel("profile")}
-            >
-              <User />
-              
-            </button>
-  
-            <button
-              className="flex items-center justify-center w-12 h-12 rounded-lg hover:bg-gray-800"
-              onClick={() => togglePanel("filter")}
-            >
-              <Menu />
-            </button>
-  
-            <button 
-                className="flex items-center justify-center w-12 h-12 rounded-lg hover:bg-gray-800"
-                onClick={() => togglePanel("info")}
-            >
-              <Info />
-            </button>
-  
-          </div>
+      const profile = await getUserProfile(authUser.id);
+      setUser(profile);
+    };
+    fetchUser();
+  }, []);
+
+  function togglePanel(panel) {
+    setActivePanel(prev => (prev === panel ? null : panel));
+  }
+
+  return (
+    <div className="flex h-screen relative">
+
+      {/* SIDEBAR */}
+      <div className="w-20 bg-[var(--color-mend-dark)] text-white flex flex-col items-center py-6">
+        <div className="flex flex-col space-y-6 w-full items-center">
+
+          <button
+            className="flex items-center justify-center w-12 h-12 rounded-full hover:bg-mend-cyan transition duration-250"
+            onClick={() => togglePanel("profile")}
+          >
+            <User />
+
+          </button>
+
+          <button
+            className="flex items-center justify-center w-12 h-12 rounded-full hover:bg-mend-cyan transition duration-250"
+            onClick={() => togglePanel("filter")}
+          >
+            <Menu />
+          </button>
+
+          <button
+            className="flex items-center justify-center w-12 h-12 rounded-full hover:bg-mend-cyan transition duration-250"
+            onClick={() => togglePanel("info")}
+          >
+            <Info />
+          </button>
+
         </div>
-  
-        {/* EXTRA SIDEBAR */}
-        {activePanel && (
-          <div className="w-64 bg-white text-black shadow-lg p-6">
-            {activePanel === "profile" && 
-            (<> 
+      </div>
+
+      {/* EXTRA SIDEBAR */}
+      {activePanel && (
+        <div className="w-64 bg-white text-black shadow-lg p-6">
+          {activePanel === "profile" &&
+            (<>
               <div>
                 <h1>Welcome, {user?.email}</h1>
                 <h1>Name: {user?.name}</h1>
@@ -67,12 +67,12 @@ export default function Sidebar() {
                 <h1>Email: {user?.email}</h1>
               </div>
             </>)
-            }
-            {activePanel === "filter" && <h1>Filter Panel</h1>}
-            {activePanel === "info" && (
-              <>
-                <div>
-                  <h1>
+          }
+          {activePanel === "filter" && <h1>Filter Panel</h1>}
+          {activePanel === "info" && (
+            <>
+              <div>
+                <h1>
 
                   Life gets busy. Sometimes you need help.
                   Sometimes you have the skills to help someone else.
@@ -82,13 +82,13 @@ export default function Sidebar() {
 
                   <br />
                   Whether it’s tutoring, moving help, design work, tech support, or everyday tasks, you can:
-                  </h1>
-                </div>
-              </>
-            )}
-          </div>
-        )}
-  
-      </div>
-    );
-  }
+                </h1>
+              </div>
+            </>
+          )}
+        </div>
+      )}
+
+    </div>
+  );
+}
